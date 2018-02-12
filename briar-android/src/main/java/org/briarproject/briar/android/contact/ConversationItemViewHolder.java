d58775850add24d8609035d7b1437327cf4f624f
Philippe Beaudry <philippe.beaudry96@gmail.com>
2018-02-11 21:54:31 -0500

    Added condition to decode base64 image and set the proper view when an image is received in the conversation

--- briar-android/src/main/java/org/briarproject/briar/android/contact/ConversationItemViewHolder.java
+++ briar-android/src/main/java/org/briarproject/briar/android/contact/ConversationItemViewHolder.java
@@ -1,10 +1,15 @@
 package org.briarproject.briar.android.contact;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.CallSuper;
 import android.support.annotation.UiThread;
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
@@ -19,24 +24,40 @@ class ConversationItemViewHolder extends ViewHolder {
 	protected final ViewGroup layout;
 	private final TextView text;
 	private final TextView time;
+	private final ImageView imageView;
 
 	ConversationItemViewHolder(View v) {
 		super(v);
 		layout = v.findViewById(R.id.layout);
 		text = v.findViewById(R.id.text);
 		time = v.findViewById(R.id.time);
+		imageView = v.findViewById(R.id.imageView);
 	}
 
 	@CallSuper
 	void bind(ConversationItem item) {
 		if (item.getBody() == null) {
 			text.setText("\u2026");
-		} else {
-			text.setText(StringUtils.trim(item.getBody()));
+		}else {
+			//check if the body string is a base64 encode image
+			if(item.getBody().startsWith("ImageTag:")){
+
+			    String encodedString = item.getBody().substring(9);
+
+				byte[] imageBytes = android.util.Base64.decode(encodedString, android.util.Base64.DEFAULT);
+				Bitmap decodeImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+
+                text.setText("Image:");
+				imageView.setImageBitmap(decodeImage);
+
+			}
+			//Displays the normal text if the string isn't a base64 image
+			else{
+				text.setText(StringUtils.trim(item.getBody()));
+			}
 		}
 
 		long timestamp = item.getTime();
 		time.setText(UiUtils.formatDate(time.getContext(), timestamp));
 	}
-
 }
