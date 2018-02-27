063f66287f7406f5791273c5ee7e84568a7a8d5f
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2018-02-26 20:17:58 -0500

    Extending BaseFragment instead of Fragment to create a constructor so that the ProfileFragment is called from the NavDrawerActivity when clicked on.

--- briar-android/src/main/java/org/briarproject/briar/android/profile/ProfileFragment.java
+++ briar-android/src/main/java/org/briarproject/briar/android/profile/ProfileFragment.java
@@ -8,11 +8,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import org.briarproject.briar.R;
+import org.briarproject.briar.android.activity.ActivityComponent;
+import org.briarproject.briar.android.fragment.BaseFragment;
 
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends BaseFragment {
 
-    @Override
+	@Override
+	public String getUniqueTag() {
+		return null;
+	}
+
+	@Override
+	public void injectFragment(ActivityComponent component) {
+
+	}
+
+	@Override
     public void onAttach(Context context) {
         super.onAttach(context);
     }
@@ -20,7 +32,22 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+	    getActivity().setTitle(R.string.title_activity_profile);
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+	public void onStart() {
+		super.onStart();
+    }
+
+    @Override
+	public void onStop() {
+    	super.onStop();
+    }
+
+    public static ProfileFragment newInstance() {
+	    return new ProfileFragment();
     }
 }
 
