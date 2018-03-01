9c3f2f79bc8843bc35287d35f48aee6cfe925207
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2018-03-01 11:53:06 -0500

    Created ImageAdapter class to store avatars in an array. Methods from ImageAdapter is then used in onCreate to set the GridView in the ProfileActivity page. No images are displayed yet. #39

--- briar-android/src/main/java/org/briarproject/briar/android/profile/ProfileActivity.java
+++ briar-android/src/main/java/org/briarproject/briar/android/profile/ProfileActivity.java
@@ -8,6 +8,14 @@ import org.briarproject.briar.R;
 import org.briarproject.briar.android.activity.ActivityComponent;
 import org.briarproject.briar.android.activity.BriarActivity;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 public class ProfileActivity extends BriarActivity {
 
     @Override
@@ -20,6 +28,13 @@ public class ProfileActivity extends BriarActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
         setContentView(R.layout.activity_profile);
+
+	    GridView gridView = findViewById(R.id.grid_view);
+	    gridView.setAdapter(new ImageAdapter(this));
+
+	    gridView.setOnItemClickListener((parent, v, position, id) -> Toast.makeText(getBaseContext(),
+			    "pic" + (position + 1) + " selected",
+			    Toast.LENGTH_SHORT).show());
     }
 
     @Override
@@ -40,6 +55,53 @@ public class ProfileActivity extends BriarActivity {
         }
         return false;
     }
+
+	public class ImageAdapter extends BaseAdapter
+	{
+		private Context context;
+		private Integer[] mThumbIds = {
+				R.drawable.ic_android_black_24dp,
+				R.drawable.ic_sentiment_very_satisfied_black_24dp,
+				R.drawable.ic_mood_black_24dp,
+		};
+
+		public ImageAdapter(Context c)
+		{
+			context = c;
+		}
+
+		//---returns the number of images---
+		public int getCount() {
+			return mThumbIds.length;
+		}
+
+		//---returns the ID of an item---
+		public Object getItem(int position) {
+			return null;
+		}
+
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		//---returns an ImageView view---
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ImageView imageView;
+			if (convertView == null) {
+				// if it's not recycled, initialize some attributes
+				imageView = new ImageView(context);
+				imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+				imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+				imageView.setPadding(8, 8, 8, 8);
+			} else {
+				imageView = (ImageView) convertView;
+			}
+
+			imageView.setImageResource(mThumbIds[position]);
+			return imageView;
+		}
+
+	}
 
 }
 
