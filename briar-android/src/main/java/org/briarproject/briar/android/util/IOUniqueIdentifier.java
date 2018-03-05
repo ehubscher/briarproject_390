ee0fc41a88f56c6dbb94f500a4eff63939be1786
Winterhart <m.user.work@gmail.com>
2018-03-04 22:21:24 -0500

    Writting the unique ID into a file in Documents sub task of issue #37

--- briar-android/src/main/java/org/briarproject/briar/android/util/IOUniqueIdentifier.java
+++ briar-android/src/main/java/org/briarproject/briar/android/util/IOUniqueIdentifier.java
@@ -1,13 +1,19 @@
 package org.briarproject.briar.android.util;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.util.Random;
 import java.util.logging.Logger;
 
-import android.content.Context;
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+
 
 /**
  * Created by Winterhart on 3/3/2018.
@@ -16,61 +22,53 @@ import android.os.Environment;
  */
 
 public class IOUniqueIdentifier {
-
-    /**
-     * This methode shall only create a file if the file doesn't already exist...
-     * @param uniqueIdentifier
-     */
     private static final Logger LOG =
             Logger.getLogger(IOUniqueIdentifier.class.getName());
+    //TODO: Find a better place to store the config file...
     private final File path =
             Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_DOCUMENTS + "/briar/"
+                    Environment.DIRECTORY_DOCUMENTS
             );
-    private File fileToCreate = new File(path, "config.txt");
+    private File fileToCreate = new File(path, "UniqueID.txt");
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    /**
+     * The constructor is handling the construction of the file and unique ID in it...
+     */
+    public IOUniqueIdentifier(Activity activity){
 
-
-    public void WriteUniqueIdToFile(String uniqueIdentifier) throws IOException {
-        boolean SuccessCreateDir;
-        if(path.exists()){
-            SuccessCreateDir = true;
-            if(fileToCreate.exists()){
-                LOG.info("***WARNING *** Unique ID , File is already created");
-                return;
+        // If the uniqueID doesn't exist build it
+        if(!DoesItExist()){
+            String unique = GenerateUniqueID();
+            try {
+                WriteUniqueIdToFile(unique, activity);
+            } catch (IOException e) {
+                LOG.info(e.getMessage());
             }
-        }else{
-            SuccessCreateDir = path.mkdir();
-            LOG.info("CREATING THE FOLDER briar in Documents");
         }
-
-
-        if(SuccessCreateDir){
-            FileOutputStream fileOutputStream = null;
-            OutputStreamWriter outputStreamWriter = null;
-
+    }
+    /**
+     * This method is writing the UniqueID into a file inside Android/Documents/config.txt
+     * @param uniqueIdentifier A unique String ID received by the method
+     * @throws IOException : may be throw
+     */
+    private void WriteUniqueIdToFile(String uniqueIdentifier, Activity activity) throws IOException {
+        // Prevent any too small string of being inserted...
+        if(uniqueIdentifier.isEmpty() | uniqueIdentifier.length() < 9)return;
+        if(!DoesItExist()){
+            verifyStoragePermissions(activity);
+            FileWriter fileWriter;
             try{
-                boolean SuccessCreateFile = fileToCreate.createNewFile();
-                if(!SuccessCreateFile){
-                    LOG.info("*** WARNING *** CAN'T Create the file, config.txt in Documents/briar folder");
-                    return;
-                }
-                fileOutputStream = new FileOutputStream(fileToCreate);
-                outputStreamWriter = new OutputStreamWriter(fileOutputStream);
-                outputStreamWriter.append(uniqueIdentifier);
-
+                fileWriter = new FileWriter(fileToCreate);
+                fileWriter.append(uniqueIdentifier);
+                fileWriter.close();
             }catch (IOException ee){
-                LOG.info(ee.getMessage());
+                LOG.info("FROM IOUniqueIdentifier " + ee.getMessage());
             }
-            finally {
-                if(outputStreamWriter != null)outputStreamWriter.close();
-                if(fileOutputStream != null){
-                    fileOutputStream.flush();
-                    fileOutputStream.close();
-                }
-            }
-
         }
-
     }
 
     /**
@@ -82,4 +80,83 @@ public class IOUniqueIdentifier {
             LOG.info("*** WARNING *** Directory Documents/briar has been deleted");
         }
     }
+
+    /**
+     * Helper function, Check if the file exist
+     * @return True is it's exist
+     */
+    private Boolean DoesItExist(){
+        if(path.exists() & fileToCreate.exists()){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Getting the String inside the text file...
+     * @return
+     */
+    public String GetUniqueID(){
+        String uniqueID = "";
+        BufferedReader reader;
+        if(DoesItExist()){
+            try{
+                reader = new BufferedReader(new FileReader(fileToCreate));
+                StringBuilder dataOut = new StringBuilder();
+                String buffer = "";
+
+                while ( (buffer = reader.readLine()) != null){
+                    dataOut.append(buffer);
+                }
+
+                uniqueID = dataOut.toString();
+                reader.close();
+
+            }catch (IOException ee){
+                LOG.info(ee.getMessage());
+            }
+        }
+        return uniqueID;
+    }
+
+    /**
+     * This method is commonly used to created unique String
+     * it is based on: https://dzone.com/articles/generate-random-alpha-numeric
+     * @return
+     */
+    public static String GenerateUniqueID(){
+        String unique = "";
+        String allPossibleChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder uniqueBuilder = new StringBuilder();
+        Random ran = new Random();
+        while (uniqueBuilder.length() < 20){
+            int i = (int) (ran.nextFloat() * allPossibleChar.length());
+            uniqueBuilder.append(allPossibleChar.charAt(i));
+        }
+        unique = uniqueBuilder.toString();
+        return unique;
+
+    }
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
 }
---
+++ briar-android/src/test/java/org/briarproject/briar/android/util/IOUniqueIdentifierTest.java
@@ -0,0 +1,34 @@
+package org.briarproject.briar.android.util;
+import junit.framework.Assert;
+import org.junit.Test;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Winterhart on 3/4/2018.
+ * This class is made to test most method possible contained in IOUniqueIdentifier.java
+ *  We cannot test all method since their relied on having a /Documents/briar folder on an android device...
+ */
+
+public class IOUniqueIdentifierTest {
+
+    /**
+     * In this test we are trying to verify if our unique ID is random enough to never get duplicate
+     * id , even if it is possible , we can reduce the risk by testing the function random id...
+     */
+    @Test
+    public void GenerateUniqueIDTest(){
+        boolean added = false;
+        Set<String> allGeneratedUniqueID = new HashSet<>();
+        int numberOfUniqueIdToCreate = 0;
+        int maxIDGenerated = 10000;
+        while (numberOfUniqueIdToCreate < maxIDGenerated){
+            added = allGeneratedUniqueID.add(IOUniqueIdentifier.GenerateUniqueID());
+            if(!added)break;
+            numberOfUniqueIdToCreate++;
+        }
+        Assert.assertTrue(added);
+    }
+
+    // TODO : Find a way to mock the android ENVIRONMENT to test the other method of IOUniqueIdentifier
+}
