f6b14e546706be16547b3b60c68dfb6d4cc2467d
Benjamin T <Benjamin.tanguay@gmail.com>
2018-03-18 02:42:39 -0400

    Created the low level logic to handle requests #105 Command design pattern with unit of work. Tasks are fed to the unit of work. Tasks call the handler to perform insertion, modification and deletion in the Identity map

---
+++ briar-webserver/src/main/java/com/briar/server/services/tasks/InsertNewUser.java
@@ -0,0 +1,41 @@
+package com.briar.server.services.tasks;
+
+import com.briar.server.exception.*;
+import com.briar.server.handler.UserHandler;
+import com.briar.server.mapper.UserMapper;
+import com.briar.server.model.domainmodelclasses.User;
+
+public class InsertNewUser extends AbstractUserTask {
+
+    public InsertNewUser(User userToAdd, UserHandler handler, UserMapper mapper) {
+        super(userToAdd, handler, mapper);
+    }
+
+    @Override
+    public void commitDB() throws DBException {
+        try {
+            userMapper.addNewUser(this.user);
+        } catch (Exception e) {
+            throw new DBException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void commitIdentityMap() throws ObjectDeletedException, ObjectAlreadyExistsException {
+        handler.add();
+    }
+
+    @Override
+    public void revertDB() throws DBException {
+        try {
+            userMapper.removeUser(this.user);
+        } catch (Exception e) {
+            throw new DBException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void revertIdentityMap() throws ObjectDeletedException, ObjectAlreadyExistsException {
+        handler.remove();
+    }
+}
---
+++ briar-webserver/src/main/java/com/briar/server/services/tasks/ModifyUser.java
@@ -0,0 +1,47 @@
+package com.briar.server.services.tasks;
+
+import com.briar.server.exception.*;
+import com.briar.server.handler.UserHandler;
+import com.briar.server.mapper.UserMapper;
+import com.briar.server.model.domainmodelclasses.User;
+
+public class ModifyUser extends AbstractUserTask {
+
+    private User oldUser;
+    private UserHandler oldUserHandler;
+
+    public ModifyUser(User newUser, User oldUser, UserHandler handler, UserHandler oldUserHandler, UserMapper mapper) {
+        super(newUser, handler, mapper);
+        this.oldUser = oldUser;
+        this.oldUserHandler = oldUserHandler;
+    }
+
+
+    @Override
+    public void commitDB() throws DBException {
+        try {
+            userMapper.modifyUser(this.user);
+        } catch (Exception e) {
+            throw new DBException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void commitIdentityMap() throws ObjectDeletedException, ObjectAlreadyExistsException {
+        this.handler.modify();
+    }
+
+    @Override
+    public void revertDB() throws DBException {
+        try {
+            userMapper.modifyUser(this.oldUser);
+        } catch (Exception e) {
+            throw new DBException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void revertIdentityMap() throws ObjectDeletedException, ObjectAlreadyExistsException {
+        this.oldUserHandler.modify();
+    }
+}
