f2cc558fd47d1b78fbb129ba0c54c13f58274ff6
Winterhart <m.user.work@gmail.com>
2018-03-25 19:30:52 -0400

    Migration of the Mock test to the package briar-android, it includes mockito framework

--- bramble-core/src/test/java/org/briarproject/bramble/restClient/BServerServicesImplTest.java
+++ bramble-core/src/test/java/org/briarproject/bramble/restClient/BServerServicesImplTest.java
@@ -2,11 +2,9 @@ package org.briarproject.bramble.restClient;
 
 
 import org.briarproject.bramble.restClient.ServerObj.SavedUser;
-import org.jmock.auto.Mock;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
-
 import java.util.Random;
 
 /**
@@ -89,41 +87,7 @@ public class BServerServicesImplTest {
 		boolean e = services.updateUserNetworkInfo(user);
 		Assert.assertTrue(e);
 	}
-	/**
-	 * This test is testing obtaining a user, with mocking the server
-	 */
-	@Test
-	public void mockObtainUserInfoTest(){
-    //TODO: Implements test using Mocking framework
-	}
 
-    /**
-     * This test is testing updating Tcp, with mocking the server
-     */
-    @Test
-    public void mockUpdateUserTcpTest(){
-
-        // TODO: Implements test updateTCP using Mocking framework
-    }
-
-    /**
-     * This test is testing updating settings, with mocking the server
-     */
-    @Test
-    public void mockUpdateUserSettingsTest(){
-
-        // TODO: Implements test updateTCP using Mocking framework
-    }
-
-    /**
-     * This test is testing creating user, with mocking the server
-     */
-    @Test
-    public void mockCreateNewUserTest(){
-
-    //TODO: Implements create a user, with mocking the server
-
-    }
 
 	private String generateFakeUserName(){
 		String fakeUserName = "";
---
+++ briar-android/src/test/java/org/briarproject/briar/android/BServerServicesWithMockTest.java
@@ -0,0 +1,48 @@
+package org.briarproject.briar.android;
+
+import org.junit.Test;
+import org.mockito.*;
+
+/**
+ * Created by Winterhart on 3/25/2018.
+ * This class is made to test the server using Mockito
+ * and by mocking the server
+ */
+
+public class BServerServicesWithMockTest {
+    /**
+     * This test is testing obtaining a user, with mocking the server
+     */
+    @Test
+    public void mockObtainUserInfoTest(){
+
+    }
+
+    /**
+     * This test is testing updating Tcp, with mocking the server
+     */
+    @Test
+    public void mockUpdateUserTcpTest(){
+
+        // TODO: Implements test updateTCP using Mocking framework
+    }
+
+    /**
+     * This test is testing updating settings, with mocking the server
+     */
+    @Test
+    public void mockUpdateUserSettingsTest(){
+
+        // TODO: Implements test updateTCP using Mocking framework
+    }
+
+    /**
+     * This test is testing creating user, with mocking the server
+     */
+    @Test
+    public void mockCreateNewUserTest(){
+
+        //TODO: Implements create a user, with mocking the server
+
+    }
+}
