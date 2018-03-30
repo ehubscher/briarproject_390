4091da68e8cbafa68018b0bfe02e0e150db2351e
Winterhart <m.user.work@gmail.com>
2018-03-30 16:08:50 -0400

    #130 , force Travis to fail by inserting a failing test

--- bramble-core/src/test/java/org/briarproject/bramble/restClient/IpifyServicesTest.java
+++ bramble-core/src/test/java/org/briarproject/bramble/restClient/IpifyServicesTest.java
@@ -21,6 +21,8 @@ public class IpifyServicesTest {
         String pattern = "^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
         String returnedIp = IpifyServices.getPublicIpOfDevice();
         boolean result = returnedIp.matches(pattern);
-        Assert.assertTrue(result);
+        // Let's change this test so we are sure it is failing,
+        // Then we will modify Travis to Send info on the failing test
+        Assert.assertTrue(false);
     }
 }
