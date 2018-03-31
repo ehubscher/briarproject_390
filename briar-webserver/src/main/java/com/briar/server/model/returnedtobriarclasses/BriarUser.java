b5d5e3efd3bb574561e14d5a03e5fe95fd3ebb42
Benjamin T <Benjamin.tanguay@gmail.com>
2018-03-30 23:01:40 -0400

    Modified existing classes so that avatar and status id are retrievable from the server #120

--- briar-webserver/src/main/java/com/briar/server/model/returnedtobriarclasses/BriarUser.java
+++ briar-webserver/src/main/java/com/briar/server/model/returnedtobriarclasses/BriarUser.java
@@ -4,11 +4,16 @@ public class BriarUser {
     private String userName;
     private String ip;
     private int port;
+    private int statusId;
+    private int avatarId;
 
-    public BriarUser(String userName, String ip, int port) {
+    public BriarUser(String userName, String ip, int port, int statusId,
+                     int avatarId) {
         this.userName = userName;
         this.ip = ip;
         this.port = port;
+        this.statusId = statusId;
+        this.avatarId = avatarId;
     }
 
     public String getUserName() {
@@ -35,12 +40,30 @@ public class BriarUser {
         this.port = port;
     }
 
+    public void setStatusId(int statusId) {
+        this.statusId = statusId;
+    }
+
+    public int getStatusId() {
+        return statusId;
+    }
+
+    public void setAvatarId(int avatarId) {
+        this.avatarId = avatarId;
+    }
+
+    public int getAvatarId() {
+        return avatarId;
+    }
+
     @Override
     public String toString() {
         return "BriarUser{" +
                 "userName='" + userName + '\'' +
                 ", ip='" + ip + '\'' +
-                ", port='" + port + '\'' +
+                ", port=" + port +
+                ", statusId=" + statusId +
+                ", avatarId=" + avatarId +
                 '}';
     }
 }
