d325e9bf880f20903be53471591db10f0ccb62c3
cynthiac3 <cynthia.cherrier@hotmail.com>
2018-04-06 21:32:34 -0400

    Attempt to add pinned messages as new EmojiTextView in the RelativeLayout of PinnedMessagesActivity #139

--- briar-android/src/main/java/org/briarproject/briar/android/pinnedmessages/PinnedMessagesActivity.java
+++ briar-android/src/main/java/org/briarproject/briar/android/pinnedmessages/PinnedMessagesActivity.java
@@ -3,21 +3,21 @@ package org.briarproject.briar.android.pinnedmessages;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
 import android.view.MenuItem;
-import android.view.View;
+
+import android.widget.RelativeLayout;
 
 import org.briarproject.bramble.api.contact.ContactId;
-import org.briarproject.bramble.api.db.DbException;
-import org.briarproject.bramble.api.sync.MessageId;
+
 import org.briarproject.briar.R;
 import org.briarproject.briar.api.messaging.MessagingManager;
+import org.thoughtcrime.securesms.components.emoji.EmojiTextView;
 
-import java.util.Collection;
 
 import javax.inject.Inject;
 
@@ -43,18 +43,28 @@ public class PinnedMessagesActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        for (int i = 0; i < messages.length; i++){
-            //TODO display the messages here this is only for test
-            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-            alertDialog.setTitle("Test");
-            alertDialog.setMessage(messages[i]);
-            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                    new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    });
-            alertDialog.show();
+        //Add pinned messages to the layout
+        if(this.findViewById(R.id.layout_pinned) != null) {
+            RelativeLayout layout = (RelativeLayout) this.findViewById(R.id.layout_pinned);
+
+            for (int i = 0; i < messages.length; i++) {
+                EmojiTextView text = new EmojiTextView(this);
+                text.setText(messages[i]);
+
+                layout.addView(text);
+
+                //TODO display the messages here this is only for test
+                /*AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+                alertDialog.setTitle("Test");
+                alertDialog.setMessage(messages[i]);
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();*/
+            }
         }
     }
 
