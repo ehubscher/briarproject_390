fb071c5f0b054798ace78b1634a76bda5480a6ba
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2018-04-09 15:14:05 -0400

    Moved ImageAdapter, references to wallpaper and clickListener to fragment because fragment was overlapping the activity #151

--- briar-android/src/main/java/org/briarproject/briar/android/wallpaper/WallpaperActivity.java
+++ briar-android/src/main/java/org/briarproject/briar/android/wallpaper/WallpaperActivity.java
@@ -1,33 +1,13 @@
 package org.briarproject.briar.android.wallpaper;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.Toast;
-
 import org.briarproject.briar.R;
 
 
 public class WallpaperActivity extends AppCompatActivity {
-
-    private Integer[] wallpapers = {
-            R.drawable.bubbles,
-            R.drawable.flower,
-            R.drawable.hellokitty,
-            R.drawable.lake,
-            R.drawable.sunset,
-            R.drawable.nebula
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,27 +21,6 @@ public class WallpaperActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
-        GridView gridView = (GridView) findViewById(R.id.gridview_wallpaper);
-        gridView.setAdapter(new ImageAdapter(this));
-
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent,
-                                    View v, int position, long id)
-            {
-                Toast.makeText(getBaseContext(),
-                        "pic" + (position + 1) + " selected",
-                        Toast.LENGTH_SHORT).show();
-                saveWallpaper(position+1);
-            }
-        });
-
-    }
-
-    public void saveWallpaper(int wallpaperId){
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putInt("wallpaper", wallpaperId);
-        editor.apply();
     }
 
     @Override
@@ -71,44 +30,6 @@ public class WallpaperActivity extends AppCompatActivity {
             return true;
         }
         return false;
-    }
-
-    public class ImageAdapter extends BaseAdapter {
-        private Context context;
-
-        public ImageAdapter(Context c) {
-            context = c;
-        }
-
-        //number of images
-        public int getCount() {
-            return wallpapers.length;
-        }
-
-        //id of an item
-        public Object getItem(int position) {
-            return position;
-        }
-
-        public long getItemId(int position) {
-            return position;
-        }
-
-        //ImageView view 
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageView;
-            if (convertView == null) {
-                imageView = new ImageView(context);
-                imageView.setLayoutParams(new GridView.LayoutParams(185, 185));
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                imageView.setPadding(5, 5, 5, 5);
-            }
-            else {
-                imageView = (ImageView) convertView;
-            }
-            imageView.setImageResource(wallpapers[position]);
-            return imageView;
-        }
     }
 
 }
--- briar-android/src/main/java/org/briarproject/briar/android/wallpaper/WallpaperFragment.java
+++ briar-android/src/main/java/org/briarproject/briar/android/wallpaper/WallpaperFragment.java
@@ -1,15 +1,32 @@
 package org.briarproject.briar.android.wallpaper;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.briarproject.briar.R;
 
 
 public class WallpaperFragment extends Fragment {
+
+    private Integer[] wallpapers = {
+            R.drawable.bubbles,
+            R.drawable.flower,
+            R.drawable.hellokitty,
+            R.drawable.lake,
+            R.drawable.sunset,
+            R.drawable.nebula
+    };
 
     public WallpaperFragment() {
     }
@@ -17,6 +34,67 @@ public class WallpaperFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_wallpaper, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_wallpaper, container, false);
+
+        GridView gridView = (GridView) view.findViewById(R.id.gridview_wallpaper);
+        gridView.setAdapter(new WallpaperFragment.ImageAdapter(view.getContext()));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent,
+                                    View v, int position, long id)
+            {
+                Toast.makeText(getActivity(),
+                        "Wallpaper saved",
+                        Toast.LENGTH_SHORT).show();
+                saveWallpaper(position+1);
+            }
+        });
+        return view;
+    }
+
+    public void saveWallpaper(int wallpaperId){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this.getActivity().getApplicationContext());
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("wallpaper", wallpaperId);
+        editor.apply();
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context context;
+
+        public ImageAdapter(Context c) {
+            context = c;
+        }
+
+        //number of images stored in the array
+        public int getCount() {
+            return wallpapers.length;
+        }
+
+        //Id of an item inside the gridview
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        //returns the ImageView view
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                imageView = new ImageView(context);
+                imageView.setLayoutParams(new GridView.LayoutParams(185, 185));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(5, 5, 5, 5);
+            }
+            else {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setImageResource(wallpapers[position]);
+            return imageView;
+        }
     }
 }
